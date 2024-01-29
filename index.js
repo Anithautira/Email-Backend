5f10f552b1ea74b402576a420e4526e3c82ee345
Anithautira <142482568+Anithautira@users.noreply.github.com>
2024-01-29 20:13:48 +0800

    commit

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ app.use(cors())
 app.use(express.json())
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD; 
-
-const dbUrl = `mongodb+srv://${USERNAME}:${PASSWORD}@gmailclone.jguf4dm.mongodb.net/?retryWrites=true&w=majority`
+const dbUrl = 'mongodb+srv://rohit10231:rohitkaranpujari@cluster0.kjynvxt.mongodb.net/?retryWrites=true&w=majority'
 const client = new MongoClient(dbUrl)
+//const dbUrl = `mongodb+srv://${USERNAME}:${PASSWORD}@gmailclone.jguf4dm.mongodb.net/?retryWrites=true&w=majority`
+//const client = new MongoClient(dbUrl)
 const port = 6000
 
 // getting all emails from all emails collection
